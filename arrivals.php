098eeb06336d9cad23e0d22d76e8e7954dc70ed7
Javed Khan <yousafzaijaved@gmail.com>
2018-07-13 17:36:16 +0200

    changes pushed on 13 July

--- arrivals.php
+++ arrivals.php
@@ -47,8 +47,7 @@
       <div class="row">
         <div class="col-md-4 col-sm-4">
 				<p class="h52">Arrival Overview</p>
-
-
+			
         </div>
 
 
@@ -108,11 +107,78 @@
 			</div>
 		</div>
 
-		<div class="icon-info">  <P>
-			Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It's also called placeholder (or filler) text. It's a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the classical author and philosopher Cicero. Its words and letters have been changed by addition or removal, so to deliberately render its content nonsensical; it's not genuine, correct, or comprehensible Latin anymore. While lorem ipsum's still resembles classical Latin, it actually has no meaning whatsoever. As Cicero's text doesn't contain the letters K, W, or Z, alien to latin, these, and others are often inserted randomly to mimic the typographic appearence of European languages, as are digraphs not to be found in the original.
+		<?php
+			
+			// Get the country names in country array
+			$country = array();
 
-			In a professional context it often happens that private or corporate clients corder a publication to be made and presented with the actual content still not being ready. Think of a news blog that's filled with content hourly on the day of going live. However, reviewers tend to be distracted by comprehensible content, say, a random text copied from a newspaper or the internet. The are likely to focus on the text, disregarding the layout and its elements. Besides, random text risks to be unintendedly humorous or offensive, an unacceptable risk in corporate environments. Lorem ipsum and its many variants have been employed since the early 1960ies, and quite likely since the sixteenth century. </P>
-            </div>
+
+			/*	CONNECT TO DATABASE*/
+      /*
+			 $db = mysqli_connect('127.0.0.1','root', '','europe_rm')
+			 or die('Error connecting to MySQL server.');
+
+			//Get the list of countruies of Arrivals
+			$query = "SELECT distinct coA FROM `arrivals`";
+			mysqli_query($db, $query) or die('Error querying database.');
+
+			$result = mysqli_query($db, $query);
+			$row = mysqli_fetch_array($result);
+
+			
+			while ($row = mysqli_fetch_array($result)) {
+			//echo $row['CoA']."</br>";
+			$country[]=$row[0];
+			*/
+			
+      $month= array ('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec');
+      
+       $db = mysqli_connect('127.0.0.1','root', '','europe_rm')
+       or die('Error connecting to MySQL server.');
+
+              
+        for ($i=0; $i <=count($month); $i++) 
+          {
+
+             
+              //Get arrival data for all the countries for all years - group by month
+            $query = "SELECT year, CoA, sum(total_nationality) FROM `arrivals` where month = '".$month[$i]."' group by year, coa order by year, coa";
+
+            mysqli_query($db, $sql) or die('Error querying database.');       
+
+            $result = mysqli_query($db, $sql);
+            $row = mysqli_fetch_array($result);
+            echo '<table><tr>';
+            while ($row = mysqli_fetch_array($result)) 
+                {
+                    echo '<tr><td>'.$row[0].'</td><td>'.$row[1].'</td><td>'.$row[2].'</td><td>'.$row[3].'</td></tr>';
+                }
+           }
+           			
+      echo '</table>';
+      print_r($year);
+			/* get the difference of the tow numbers*/
+
+
+
+			$val = round(getDifference (30000, 10000),1).'%';
+
+
+			/* Function to calculate the difference between the two figures*/	
+			function getDifference($oldValue, $newValue)
+			{
+			$diff = $newValue - $oldValue;
+			$percentageDiff = ($diff/$oldValue)*100;
+			return $percentageDiff;    
+			}
+
+		?>
+
+
+
+		<div class="icon-info">
+      <p> end of results</p>
+		</div>
 
 	</div>
 	
